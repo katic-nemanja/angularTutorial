7054e73af8209b427086178b5791f1e097f6b233
katic-nemanja <katic.nemanja@gmail.com>
2024-08-05 14:13:24 +0200

    poslednji commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
     {
         path: "",
-        component:HomeComponent
+        component: HomeComponent
     }
 ];
