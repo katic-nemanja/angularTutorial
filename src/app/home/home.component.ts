733f0b1e8b9caf4c5af34d883b17f208046a416b
katic-nemanja <katic.nemanja@gmail.com>
2024-08-05 02:05:51 +0200

    sada radi http request

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +11,12 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private productsService: ProductsService){}
+
+  ngOnInit(){
+    this.productsService.getProducts('http://localhost:3000/clothes', {page:0,perPage:5}).subscribe((products:Products)=>{
+      console.log(products.items);
+    })
+  }
+  
 }
